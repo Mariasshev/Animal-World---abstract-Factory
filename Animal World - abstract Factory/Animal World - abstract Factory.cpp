9a7836d1cae0f68fa6e82da30bd5ea83c577b397
Maria Shevchenko <sevcenkomaria72@gmail.com>
2024-03-31 23:20:39 +0300

    Add project files.

---
+++ Animal World - abstract Factory/Animal World - abstract Factory.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Continent
+{
+public:
+	virtual void CreateHerbivore() = 0;		//травоядное
+	virtual void CreateCarnivore() = 0;		//плотоядное
+};
+
+class Afrika
+{
+
+};
+
+class NorthAmerica
+{
+
+};
+
+class Herbivore
+{
+public:
+
+};
+
+
+int main()
+{
+
+}
